5870ad81ccde4f06abdca47b8eb08a7cc67dfab5
JAGDISH <js8724285@gmail.com>
2026-02-05 10:24:23 +0530

    [jagdish] add EJS Form render

--- EJS/server.js
+++ EJS/server.js
@@ -2,6 +2,8 @@ import express from "express"
 
 const app=express();
 app.set("view engine","ejs");
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/",(req,res)=>{
     res.render("index");
 });
@@ -20,6 +22,33 @@ app.get("/list",(req,res)=>{
    
          res.render("list",{arr});
 })
+
+let userData=[
+    {id:1,name:"amit",age:"23",password:"1233"},
+    {id:2,name:"rahul",age:"35",password:"12243"},
+    {id:3,name:"ravi",age:"25",password:"1234"},
+];
+
+app.get("/users",(req,res)=>{
+    res.render("users",{userData});
+});
+app.post("/api/users",(req,res)=>{
+    const {name,password}=req.body;
+    let newData={
+        id:userData.length+1,
+        name,
+        password
+    }
+    userData.push(newData);
+    res.redirect("/users");
+});
+app.get("/api/users/:id", (req, res) => {
+    const id =req.params.id;
+    console.log(id);
+    userData = userData.filter(user => user.id != id);
+    res.redirect("/users");
+});
+
 
 app.listen(3000,(req,res)=>{
    console.log("server is running on 3000 Port");
